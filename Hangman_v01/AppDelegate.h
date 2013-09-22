4d94a5ea4ea93ca79f4f2d1f0b050bdf252473c4
Ted <ted@Teds-Mac.local>
2013-09-21 19:27:52 -0700

    Initial Commit

---
+++ Hangman_v01/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hangman_v01
+//
+//  Created by Ted on 9/21/13.
+//  Copyright (c) 2013 Ted. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Hangman_v01/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Hangman_v01
+//
+//  Created by Ted on 9/21/13.
+//  Copyright (c) 2013 Ted. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
