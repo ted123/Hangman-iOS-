ad842b90ec9fbfaf197bb946f4cb4a1e3ea37daa
Ted <ted@Teds-Mac.local>
2013-09-24 07:53:08 -0700

    FILL IN THE BLANK FEATURE ADDED

--- Hangman_v01/ViewController2.m
+++ Hangman_v01/ViewController2.m
@@ -9,35 +9,60 @@
 #import "ViewController2.h"
 
 @interface ViewController2 ()
--(void)checkit:(NSString*)value;
-
+-(void)checkit:(unichar)value;
+-(void)generateBlanks:(NSString*)value;
+-(NSString*)placeSpaces:(NSString*)value;
 @end
 
 @implementation ViewController2
 NSArray *words;
+NSString* blankWord;
 int randomIndex;
 
-- (void)checkit:(NSString*)value{
-   
-   NSString *temp =[words objectAtIndex:randomIndex];
-   NSString *loc=@"found at: ";
-   NSRange searchRange = NSMakeRange(0,temp.length);
-   NSRange foundRange;
-    while (searchRange.location<temp.length) {
-       searchRange.length = temp.length-searchRange.location;
-        foundRange = [temp rangeOfString:value options:1 range:searchRange];
-        if(foundRange.location != NSNotFound){
-            
-            searchRange.location = foundRange.location+foundRange.length;
-            loc=[loc stringByAppendingFormat:@" %d",searchRange.location];
-        }else{
-            break;
-        }
-        
+-(NSString*)placeSpaces:(NSString *)value{
+    NSString* result=@"";
+    for (int i=0; i<value.length; i++) {
+        unichar ch = [value characterAtIndex:i];
+        result=[result stringByAppendingFormat:@"%c ",ch];
     }
-    _answer.text=loc;
-    
-    
+    return result;
+}
+
+-(void)generateBlanks:(NSString *)value{
+    _answer.text=@"";
+    for (int i=0; i<value.length; i++) {
+        unichar ch = [value characterAtIndex:i];
+        if (ch!=' ') {
+            _answer.text=[_answer.text stringByAppendingFormat:@"_"];
+        }else{
+            _answer.text=[_answer.text stringByAppendingFormat:@" "];
+        }
+            
+    }
+}
+
+- (void)checkit:(unichar)value{
+   
+
+    NSString* ans_prev=_answer.text;
+    unichar ch;
+  
+    _answer.text=@"";
+    for (int i=0; i<blankWord.length; i++) {
+            ch = [blankWord characterAtIndex:i];
+        
+            if(ch==value){
+                _answer.text=[_answer.text stringByAppendingFormat:@"%c",value];
+                
+            }else if(ch==' '){
+                _answer.text=[_answer.text stringByAppendingFormat:@" "];
+            }else if([ans_prev characterAtIndex:i]!='_') {
+                _answer.text=[_answer.text stringByAppendingFormat:@"%c",[ans_prev characterAtIndex:i]];
+            }else{
+                _answer.text=[_answer.text stringByAppendingFormat:@"_"];
+            }
+         
+    }
 
 }
 
@@ -53,9 +78,10 @@ int randomIndex;
 
 - (void)viewDidLoad
 {
-    words = [NSArray arrayWithObjects:@"ARTIST",@"BROKEN",nil];
+    words = [NSArray arrayWithObjects:@"ARTIST  SAD",@"BROKEN HEART",nil];
     randomIndex=arc4random()%2;
-
+    blankWord=[self placeSpaces:words[randomIndex]];
+    [self generateBlanks:(blankWord)];
     [super viewDidLoad];
     
 	// Do any additional setup after loading the view.
@@ -74,94 +100,94 @@ int randomIndex;
 - (IBAction)press_control:(id)sender {
     switch (_b1.selectedSegmentIndex) {
         case 0:
-                [self checkit:@"A"];
+                [self checkit:'A'];
             break;
         case 1:
-                [self checkit:@"B"];
+                [self checkit:'B'];
             break;
         case 2:
-                [self checkit:@"C"];
+                [self checkit:'C'];
             break;
         case 3:
-                [self checkit:@"D"];
+                [self checkit:'D'];
             break;
         case 4:
-                [self checkit:@"E"];
+                [self checkit:'E'];
             break;
         case 5:
-                [self checkit:@"F"];
+                [self checkit:'F'];
             break;
         case 6:
-                [self checkit:@"G"];
+                [self checkit:'G'];
             break;
         default:
             break;
     }
     switch (_b2.selectedSegmentIndex) {
         case 0:
-            [self checkit:@"H"];
+            [self checkit:'H'];
             break;
         case 1:
-            [self checkit:@"I"];
+            [self checkit:'I'];
             break;
         case 2:
-            [self checkit:@"J"];
+            [self checkit:'J'];
             break;
         case 3:
-            [self checkit:@"K"];
+            [self checkit:'K'];
             break;
         case 4:
-            [self checkit:@"L"];
+            [self checkit:'L'];
             break;
         case 5:
-            [self checkit:@"M"];
+            [self checkit:'M'];
             break;
         case 6:
-            [self checkit:@"N"];
+            [self checkit:'N'];
             break;
         default:
             break;
     }
     switch (_b3.selectedSegmentIndex) {
         case 0:
-            [self checkit:@"O"];
+            [self checkit:'O'];
             break;
         case 1:
-            [self checkit:@"P"];
+            [self checkit:'P'];
             break;
         case 2:
-            [self checkit:@"Q"];
+            [self checkit:'Q'];
             break;
         case 3:
-            [self checkit:@"R"];
+            [self checkit:'R'];
             break;
         case 4:
-            [self checkit:@"S"];
+            [self checkit:'S'];
             break;
         case 5:
-            [self checkit:@"T"];
+            [self checkit:'T'];
             break;
         case 6:
-            [self checkit:@"U"];
+            [self checkit:'U'];
             break;
         default:
             break;
     }
     switch (_b4.selectedSegmentIndex) {
         case 0:
-            [self checkit:@"V"];
+            [self checkit:'V'];
             break;
         case 1:
-            [self checkit:@"W"];
+            [self checkit:'W'];
             break;
         case 2:
-            [self checkit:@"X"];
+            [self checkit:'X'];
             break;
         case 3:
-            [self checkit:@"Y"];
+            [self checkit:'Y'];
             break;
         case 4:
-            [self checkit:@"Z"];
+            [self checkit:'Z'];
             break;
         default:
             break;
