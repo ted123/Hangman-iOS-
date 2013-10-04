25bdbb5a4385f6532a22798f4c03b321a5b69b9a
rj <rj@rjs-Mac.local>
2013-10-04 12:24:56 +0800

    Added selectChar

---
+++ Hangman_v01/selectChar.h
@@ -0,0 +1,13 @@
+//
+//  selectChar.h
+//  Hangman_v01
+//
+//  Created by rj on 10/4/13.
+//  Copyright (c) 2013 Ted. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface selectChar : UIViewController
+
+@end
